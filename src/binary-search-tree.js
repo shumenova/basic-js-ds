aef5356b561c4290cb12a6b6b9bc21b9550de583
Sulushash Shumenova <shumenova@Sulushashs-MacBook-Air.local>
2021-10-18 16:51:13 +0600

    min, max  solved

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -121,13 +121,27 @@ module.exports = class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.graph) {
+      return;
+    }
+    let node = this.graph;
+    while(node.left) {
+      node = node.left;
+    }
+
+    return node.data;
+
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.graph) {
+      return;
+    }
+    let node = this.graph; 
+    while(node.right) {
+      node= node.right
+    }
+    return node.data;
   }
 
 }
